734018128ec80c44bc3a2bc4716585b737d33ce5
Richard Mands <richard@cinderhousecreative.com>
2022-12-06 22:32:16 +0900

    Fix broken link and point to supported wallets

--- src/screens/homepage/text.jsx
+++ src/screens/homepage/text.jsx
@@ -3,11 +3,18 @@ export default {
 	Text: [
 		'MyKinWallet helps you easily interact with the Kin blockchain. It is an open-source, client-side tool and service that helps the user handle their Kin coins and move them between wallets.',
 		<div>
-			In order to use MyKinWallet you need to obtain a supported wallet (see the list of Kin-supported wallets at -{' '}
-			<a target="_blank" rel="noreferrer" href="https://www.kin.org/token-migration/" style={{ color: 'white', textDecoration: 'underline' }}>
-				https://www.kin.org/token-migration
+			In order to use MyKinWallet you need to obtain a supported wallet. <br />
+			<br />
+			Visit{' '}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.kin.org/ecosystem-apps"
+				style={{ color: 'white', textDecoration: 'underline' }}
+			>
+				kin.org/ecosystem-apps
 			</a>
-			)
+			{``}. To see the list of supported wallets, scroll down and select 'Wallets'.
 		</div>
 	]
 };
